50868d128d6e5f78729e4edcdf9e039f959b6197
Changxiang Gao <gao654@purdue.edu>
2024-02-25 15:39:32 -0500

    added cycle wires to evaluation, and method to convert adj matrix to adj list

--- graph.py
+++ graph.py
@@ -5,5 +5,15 @@ class Graph:
       self.adj_matrix = [[0] * nodes for _ in range(nodes)]
     else:
       self.adj_matrix = adj_matrix
+
+  def _adj_list_from_matrix(self):
+    self.adj_list = []
+    for i in range(len(self.adj_matrix)):
+      row = self.adj_matrix[i]
+      r = []
+      for dst in range(self.nodes):
+        r.append((dst, row[dst]))
+      self.adj_list.append(r)
+
   def add_edge(self, src: int, dst: int, cost: int):
     self.adj_matrix[src][dst] = cost
--- main.py
+++ main.py
@@ -28,11 +28,16 @@ def tsp_circuit(graph: Graph):
 Calculate the specific wires for each use (like a list)
 Currently, return the encoded_wires, aux_wire, all_wires
 """
-def evaluate_wires(graph):
+def evaluate_wires(graph: Graph):
+  cities = graph.nodes
+  choice_bit_size = int(np.ceil(np.log2(cities)))
+
+  cycle_wires = range(cities * choice_bit_size)
+
   all_wires = 10 # TODO - placeholder
   measured_wires = list(range(9)) # TODO - placeholder
   aux_wires = all_wires - 1 # in general, aux wire is usually the last wire
-  return measured_wires, aux_wires, all_wires
+  return cycle_wires, measured_wires, aux_wires, all_wires
 
 """
 Generates a random graph based on number of cities (vertices)
@@ -48,10 +53,17 @@ if __name__ == "__main__":
 
   # evaluate wires function
   # returns measured_wires, aux_wire, total_wires
-  measured_wires, aux_wires, all_wires = evaluate_wires(graph)
+  cycle_wires, measured_wires, aux_wires, all_wires = evaluate_wires(graph)
 
   # create a device based number of wires
   dev = qml.device("default.qubit", wires=all_wires)
 
   # create a qnode that is depending on circuit and device
   circuit = qml.QNode(tsp_circuit, dev)
+
+  # TODO - Generate a picture of the circuit
+
+  # Generate the probabilities of the graph
+  probs = circuit(graph)
+
+  # generate a bar chart of the probabilities
