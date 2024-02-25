be11a85552044fb8784a9d5a95e144822c06a8be
Changxiang Gao <gao654@purdue.edu>
2024-02-25 11:33:16 -0500

    aded oracle file, template for clc and hcd oracle

---
+++ oracle.py
@@ -0,0 +1,19 @@
+import pennylane as qml
+from pennylane import numpy as np
+
+"""
+Cycle Length Comparison (CLC) oracle. Marks wires if path is below threshold c_th
+Args:
+- c_th: cost threshold
+"""
+def clc_oracle(c_th: int):
+  # TODO
+  pass
+
+"""
+Hamiltonian Cycle Detection (HCD) oracle. Marks the city choices if the path forms a Hamiltonian cycle
+Requires the use of Index Forwarder qubits (I) and OR-wires
+"""
+def hcd_oracle():
+  # TODO
+  pass
